e42060da61d2287bfa9a2a8cb8a7b8f3e7d5b515
valdecir <rm.valdecir@gmail.com>
2021-06-27 17:44:53 -0300

    :bug: fix docker files
    fix project name in docker files

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t docker.io/valrai/trainix:latest --file Dockerfile .
+docker build -t docker.io/valrai/getwork:latest --file Dockerfile .
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to build: $status"
